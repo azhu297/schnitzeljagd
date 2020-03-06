a0368ce160e5d9d4a0d0b44dfad5c9cfd59d46c8
Alexander Zhu <azhu@posteo.de>
2020-03-06 13:06:49 +0100

    Add tests for LocationView

--- mapquiz/tests.py
+++ mapquiz/tests.py
@@ -1,5 +1,6 @@
 from django.db.utils import IntegrityError
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 from .models import *
 
@@ -53,3 +54,36 @@ class QuizTest(TestCase):
             self.fail(f"Raised ValidationError unexpectedly: {e.message}")
         s = Quiz(name="Quiz 3", hunt=h, stage=1)
         self.assertRaises(ValidationError, s.save)
+
+
+def generate_quiz(stage=0):
+    h = Hunt(name="Hunt")
+    h.save()
+    q = Quiz(name=f"Quiz {stage}", hunt=h, stage=stage)
+    q.save()
+    return q
+
+
+def generate_location(hint="A", stage=0):
+    loc = Location(quiz=generate_quiz(stage=stage), hint=hint, found_text="Found")
+    loc.save()
+    return loc
+
+
+class LocationViewTest(TestCase):
+    def test_valid_location(self):
+        """ Can reach a valid location by uri code"""
+        loc = generate_location()
+        url = reverse('mapquiz:location', args=(loc.uri.code,))
+        client = Client()
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)  # Valid Http Response
+
+    def test_missing_location(self):
+        """ Cannot reach a missing url """
+        # Get a non existing url by generating a new location but not saving it -> guaranteed missing url
+        l = Location(quiz=generate_quiz(), hint="H", found_text="F")
+        url = reverse('mapquiz:location', args=(l.uri.code,))
+        client = Client()
+        response = client.get(url)
+        self.assertEqual(response.status_code, 404)  # Not found
