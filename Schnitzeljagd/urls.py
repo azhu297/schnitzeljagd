e434d181f867f492db95920e2c67ac5599c89d68
Alexander Zhu <azhu@posteo.de>
2020-03-09 17:03:45 +0100

    Add media folder, for debugging purposes only

--- Schnitzeljagd/urls.py
+++ Schnitzeljagd/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('mapquiz/', include('mapquiz.urls'))
-]
+                  path('admin/', admin.site.urls),
+                  path('mapquiz/', include('mapquiz.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# TODO: media not for production use, see:
+# https://docs.djangoproject.com/en/dev/howto/static-files/#serving-files-uploaded-by-a-user-during-development
